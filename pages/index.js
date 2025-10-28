038f0741777ea9e88664437fa810622e848e682e
Rob Draper <rob.draper@live.com>
2025-10-28 18:51:33 +0000

    Moved over to Next

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// import './Index.css';
+import Header from '../components/navigation/header/Header';
+
+function Home() {
+  return (
+    <div>
+      <>
+      <Header />
+      <h1>Ad Free Video</h1>
+      </>
+    </div>
+  );
+}
+
+export default Home;
